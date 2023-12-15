30cacd38142f61807a40af879035cdc8927c7147
enrique986 <eviliguer@gmail.com>
2023-12-14 23:27:07 -0300

    nuevo formato de tipografia

--- portfolio/models.py
+++ portfolio/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.db.models.fields import CharField, URLField
 from django.db.models.fields.files import ImageField 
 
-
+        #clases que vienen           
 class Project(models.Model):
     title = CharField(max_length=100)
     description = CharField(max_length=200)
